1b4905a186e2ce03227385a6850dfb81da315f2b
thamakorn.k <thamakorn.aom31@gmail.com>
2023-06-25 21:52:24 +0700

    feat: #1 add client for grpc on calculator_service

---
+++ client/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"client/services"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+
+	//use insecure
+	creds := insecure.NewCredentials()
+	cc, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(creds))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cc.Close()
+
+	calculatorClient := services.NewCalculatorClient(cc)
+	calculatorService := services.NewCalculatorService(calculatorClient)
+
+	//call service
+	err = calculatorService.Hello("aomtest")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
--- proto/gen.sh
+++ proto/gen.sh
@@ -1,1 +1,3 @@
 protoc calculator.proto --go_out=../server --go-grpc_out=../server
+
+protoc calculator.proto --go_out=../client --go-grpc_out=../client
