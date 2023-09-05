976f28bbe24c16572ae02b870429d5043954faa5
thamakorn.k <thamakorn.aom31@gmail.com>
2023-09-06 00:21:06 +0700

    feat(server): add new method Fibonacci and implement on server service

--- server/services-proto/calculator_service.go
+++ server/services-proto/calculator_service.go
@@ -3,6 +3,7 @@ package services_proto
 import (
 	context "context"
 	"fmt"
+	"time"
 )
 
 type calculatorServer struct {
@@ -22,4 +23,32 @@ func (server calculatorServer) Hello(ctx context.Context, req *HelloRequest) (*H
 		Result: result,
 	}
 	return &response, nil
+}
+
+func (server calculatorServer) Fibonacci(req *FibonacciRequest, stream Calculator_FibonacciServer) error {
+	//return หลายค่า จาก request ที่ได้แค่ 1
+	for n := uint32(0); n <= req.N; n++ {
+		result := fib(n)
+		res := FibonacciResponse{
+			Result: result,
+		}
+		//ส่ง response ไปให้ stream
+		//1.
+		stream.Send(&res)
+		//2. ติด sleep ไว้หน่อย เดี๋ยวเร็วเกิน
+		time.Sleep(time.Second)
+	}
+
+	return nil
+}
+
+func fib(n uint32) uint32 {
+	switch n {
+	case 0:
+		return 0
+	case 1:
+		return 1
+	default:
+		return fib(n-1) + fib(n-2)
+	}
 }
